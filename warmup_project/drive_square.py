e3a58ebd1fa08e97d0822ea9f0cd5a511d5bfda3
cmawn <cmawn@olin.edu>
2024-09-15 20:27:20 -0400

    Created drive_square and teleop

---
+++ warmup_project/drive_square.py
@@ -0,0 +1,66 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from time import time
+
+
+class DriveSquareNode(Node):
+    """
+    Node that tells the neato to drive in a 1m by 1m square
+    """
+
+    def __init__(self):
+        """
+        Initializes the class
+        """
+        super().__init__("DriveSquareNode")
+
+        timer_period = 0.1
+        self.timer = self.create_timer(timer_period, self.run_loop)
+        # creates a publisher that tells the neato its wheel velocities
+        self.vel_publisher = self.create_publisher(Twist, "cmd_vel", 10)
+        self.start_time = time()
+
+    def run_loop(self):
+        """
+        tells the robot to move
+        """
+
+        vel = Twist()
+        # publish the move forward command for a set amount of time, then a turn command and repeat till theres a square
+        current_time = time()
+        delta_time = current_time - self.start_time
+        while delta_time < 10:
+            vel.linear.x = 0.1
+        while delta_time > 10 and delta_time < 20:
+            vel.linear.x = 0
+            vel.angular.z = 0.1
+        while delta_time > 20 and delta_time < 30:
+            vel.angular.z = 0
+            vel.linear.x = 0.1
+        while delta_time > 30 and delta_time < 40:
+            vel.linear.x = 0
+            vel.angular.z = 0.1
+        while delta_time > 40 and delta_time < 50:
+            vel.angular.z = 0
+            vel.linear.x = 0.1
+        while delta_time > 50 and delta_time < 60:
+            vel.linear.x = 0
+            vel.angular.z = 0.1
+        while delta_time > 60 and delta_time < 70:
+            vel.angular.z = 0
+            vel.linear.x = 0.1
+
+
+def main(args=None):
+    """
+    main function
+    """
+    rclpy.init(args=args)
+    node = DriveSquareNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
---
+++ warmup_project/teleop.py
@@ -0,0 +1,82 @@
+import tty
+import select
+import sys
+import termios
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+
+class TeleopNode(Node):
+    """
+    Node which moves the neato based on keyboard input
+    """
+
+    def __init__(self):
+        """
+        initializes the class
+        """
+        super().__init__("teleopNode")
+        # creates the timer
+        timer_period = 0.1
+        self.timer = self.create_timer(timer_period, self.run_loop)
+        # create publisher to tell the motors to move
+        self.publisher = self.create_publisher(Twist, "cmd_vel", 10)
+        # creates a publisher to see what key is being pressed, used for debugging
+        self.publisher_key = self.create_publisher(String, "key", 10)
+
+    def run_loop(self):
+        """
+        tells the robot to move when a certain key is pressed
+        """
+        # series of if statements telling the robot what to do for each key
+        vel = Twist()
+        if self.getKey() == "w":
+            vel.linear.x = 0.1
+        elif self.getKey() == "s":
+            vel.linear.x = -0.1
+        elif self.getKey() == "a":
+            vel.angular.z = 0.1
+        elif self.getKey() == "d":
+            vel.angular.z = -0.1
+        elif self.getKey() == "x":
+            vel.linear.x = 0.0
+            vel.angular.z = 0.0
+        elif self.getKey() == "/x03":
+            vel.linear.x = 0.0
+            vel.angular.z = 0.0
+            self.publisher.publish(vel)
+            rclpy.shutdown()
+
+        self.publisher.publish(vel)
+        key_data = String()
+        key_data.data = self.getKey()
+        self.publisher_key.publish(key_data)
+
+    settings = termios.tcgetattr(sys.stdin)
+    key = None
+
+    def getKey(self):
+        """
+        Gets and returns the current key input
+        """
+        tty.setraw(sys.stdin.fileno())
+        select.select([sys.stdin], [], [], 0)
+        key = sys.stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
+        return key
+
+
+def main(args=None):
+    """
+    main function
+    """
+    rclpy.init(args=args)
+    node = TeleopNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
