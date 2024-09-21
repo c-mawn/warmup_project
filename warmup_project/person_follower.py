d97122a7ec42eff9d6299b15418a721cf178ee11
cmawn <cmawn@olin.edu>
2024-09-21 13:58:13 -0400

    Rebuilt the PersonFollowerNode... code throwing errors but close to comepletion

--- warmup_project/person_follower.py
+++ warmup_project/person_follower.py
@@ -1,10 +1,9 @@
-import math
 import rclpy
+import numpy as np
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import Odometry
-from time import time
+from neato2_interfaces.msg import Bump
 
 
 class PersonFollowerNode(Node):
@@ -20,118 +19,92 @@ class PersonFollowerNode(Node):
         # initializing the class
         super().__init__("PersonFollowerNode")
 
+        # initializing the values used through the whole node
+        self.item_error = 0.0
+        self.avg_x = 0.0
+        self.avg_y = 0.0
+        self.item_distance = 0.0
+        self.bumped = False
+
         # creating the timer
         timer_period = 0.1
         self.timer = self.create_timer(timer_period, self.run_loop())
 
-        # creates publishers for the node
+        # creates publishers for the velocity to wheels
         self.vel_publisher = self.create_publisher(Twist, "cmd_vel", 10)
 
-        # creates subscriber for the lidar scanner
+        # creates subscribers for lidar and bump
         self.scan_subscriber = self.create_subscription(
-            LaserScan, "scan", self.handle_scan, 10
+            LaserScan, "scan", self.get_item_error, 10
         )
-        self.odom_subscriber = self.create_subscription(
-            Odometry, "odom", self.handle_odom, 10
+        self.bump_subscriber = self.create_subscription(
+            Bump, "bump", self.handle_bump, 10
         )
-        self.scan = LaserScan()
-        self.odom = Odometry()
 
-        # creates a world time from the start of the turn
-        self.start_turn_time = time()
-        self.start_straight_time = time()
-
-    def handle_scan(self, msg: LaserScan):
+    def get_item_error(self, msg: LaserScan):
         """
         handles every laser scan from the subscriber
         """
-        self.scan = msg
+        scans = np.array(msg.ranges)
+        ranges = np.array(range(361))
+        x = 0
+        d_to_rad = np.pi / 180
 
-    def handle_odom(self, msg: Odometry):
+        # removes scans that are too far away or returned a 0
+        while x < len(scans):
+            current_scan = scans[x]
+            if current_scan > 1.0 or current_scan <= 0:
+                scans = np.delete(scans, x)
+                ranges = np.delete(ranges, x)
+            else:
+                x += 1
+
+        if len(scans) > 0:
+            # changes the scans to cartesian coordinates
+            x_coordinates = np.multiply(scans, np.cos(np.multiply(ranges, d_to_rad)))
+            y_coordinates = np.multiply(scans, np.sin(np.multiply(ranges, d_to_rad)))
+
+            # averages the coordinates to find the center of the item
+            self.avg_x = np.average(x_coordinates)
+            self.avg_y = np.average(y_coordinates)
+
+            # math to find the distance and angle from the neato to the person
+            self.item_distance = np.sqrt(self.avg_x**2 + self.avg_y**2)
+            theta = np.arctan2(self.avg_y, self.avg_x)
+
+            self.item_error = theta
+        else:
+            self.item_error = 0.0
+
+    def handle_bump(self, msg: Bump):
         """
-        handles the odometry of the neato at every scan time
+        handles the bump state of the neato
         """
-        self.odom = msg
+        self.bumped = (
+            msg.left_front or msg.right_front or msg.left_side or msg.right_side
+        )
 
     def run_loop(self):
         """
         loop that runs every timer incriment.
         """
-        # Take scan, get edges of items in list
-        # for example, if there are zeros through index 0-25,
-        # then values from 26-30, then zeros for 31-50, i want to
-        # isolate the 26-30 indicies
-        # then get the angle compared to the neato of where those values are
-        # for example, if the neato is pointing at 0 rad, and the item is
-        # reading at -pi/4 rad from it, I want to turn the neato towards the item
-        # then move forward at one speed, for the distance that the item is away
 
-        # the way i have built this means that if the lidar scan is take close to a wall,
-        # the neato will go towards that... more specifically, the first item that the neato
-        # sees on the scan will be moved towards. I could probably do a ton of math to
-        # ignore things that are straight lines, but id like ot enjoy my life instead
-        # tldr: dont run this with other items around... only human
-
-        # current time
-        current_time = time()
-
-        # sets scan_ranges to be the list of scans from the neato
-        scan_ranges = self.scan.ranges
-
-        # sets theta_step to be the single float reprisenting the sngle incriment from the scan
-        theta_step = self.scan.angle_increment
-
-        # initializes the start and stop index of the "item" that the lidar sees
-        item_start_index = 0
-        item_stop_index = 0
-
-        for scan_range in scan_ranges:
-            # checks to see if the lidar sees an item
-            if scan_range != 0:
-                # if the lidar sees something, it sets the current index to the item start, then breaks
-                item_start_index = scan_ranges.index(scan_range)
-                break
-        for scan_range in reversed(scan_ranges):
-            # does the same thing in reverse to find the item stop index
-            if scan_range != 0:
-                item_stop_index = scan_ranges.index(scan_range)
-                break
-        # seperates the item from the zeros
-        item_range = scan_ranges[item_start_index:item_stop_index]
-        # gets the index of the center of the item
-        item_center = int(len(item_range) / 2)
-
-        # calculates the angle of this center index in the neato frame
-        orientation = self.odom.pose.pose.orientation
-        [_, _, neato_theta] = euler_from_quaternion(
-            orientation.x, orientation.y, orientation.z, orientation.w
-        )
-
-        # finds the angle of the center of the item
-        item_theta = theta_step * (item_start_index + item_center)
-        # finds the difference between the neato angle and the item angle
-        delta_theta = neato_theta - item_theta
-
+        # initializing the vel
         vel = Twist()
 
-        turn_speed = 0.2  # rad/sec
-        turn_time = delta_theta / turn_speed
-
-        self.start_turn_time = current_time
-
-        if current_time - self.start_turn_time < turn_time:
-            vel.angular.z = turn_speed
-            self.vel_publisher.publish(vel)
-        else:
+        # checks to see if neato has been bumped, if it has been, then stop moving
+        if self.bumped:
+            vel.linear.x = 0.0
             vel.angular.z = 0.0
-            done_turn = True
-            self.start_straight_time = current_time
-            self.vel_publisher.publish(vel)
-
-        if done_turn:
-            if current_time - self.start_straight_time < 4.0:
+        else:
+            # checks to see how much of a turn the person is from the neato
+            # if the turn is large enough, the linear movement stops, and it only turns
+            if abs(self.item_error) > 0.5:
+                vel.linear.x = 0.0
+            else:
                 vel.linear.x = 0.2
-                self.vel_publisher.publish(vel)
+            # sets the angular speed to be .2
+            vel.angular.z = 0.2
 
 
 def main(args=None):
@@ -146,27 +119,3 @@ def main(args=None):
 
 if __name__ == "__main__":
     main()
-
-
-# fucntions for quaternion to euler stuff
-def euler_from_quaternion(x, y, z, w):
-    """
-    Convert a quaternion into euler angles (roll, pitch, yaw)
-    roll is rotation around x in radians (counterclockwise)
-    pitch is rotation around y in radians (counterclockwise)
-    yaw is rotation around z in radians (counterclockwise)
-    """
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + y * y)
-    roll_x = math.atan2(t0, t1)
-
-    t2 = +2.0 * (w * y - z * x)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    pitch_y = math.asin(t2)
-
-    t3 = +2.0 * (w * z + x * y)
-    t4 = +1.0 - 2.0 * (y * y + z * z)
-    yaw_z = math.atan2(t3, t4)
-
-    return roll_x, pitch_y, yaw_z  # in radians
